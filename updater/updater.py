ec74684c1552429d5060f2945ffe68c9901bbfa5
Ivan Kruglov <kruglovivan005@gmail.com>
2023-02-02 09:52:59 +0300

    added updater fixed

--- updater/updater.py
+++ updater/updater.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import asyncio
 from os import system, popen
 from os.path import exists
+from aiorun import run
 
-if exists("/root/schoolDaemon"):
-    if "Your branch is up to date with 'origin/master'." not in popen(
-            "cd /root/schoolDaemon && git remote update && git status -uno").read():
-        system("cd /root/schoolDaemon && git pull && python3 setup.py 3")
-else:
-    system("cd /root && git clone https://github.com/pihta24/schoolDaemon.git")
-    system("cd /root/schoolDaemon && python3 setup.py 3")
+
+async def main():
+    while True:
+        await asyncio.sleep(200)
+        if exists("/root/schoolDaemon"):
+            if "Your branch is up to date with 'origin/master'." not in popen(
+                    "cd /root/schoolDaemon && git remote update && git status -uno").read():
+                system("cd /root/schoolDaemon && git pull && python3 setup.py 3")
+        else:
+            system("cd /root && git clone https://github.com/pihta24/schoolDaemon.git")
+            system("cd /root/schoolDaemon && python3 setup.py 3")
+        await asyncio.sleep(3400)
+
+if __name__ == "__main__":
+    run(main(), stop_on_unhandled_errors=True)
